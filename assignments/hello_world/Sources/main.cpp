7b94acee3e3df9abba71ab2788b8fc888c778c46
Gabrielly Gomes <kimalvesg@outlook.com>
2025-12-07 15:45:09 -0300

    Add main.cpp to print 'Hello, World!'

---
+++ assignments/hello_world/Sources/main.cpp
@@ -0,0 +1,13 @@
+// headers:
+
+#include <iostream> // principal library
+
+using namespace std; // to use elements from the library withtout having to define std every time
+
+// main function
+
+int main()
+{
+    cout << "Hello, World!" << endl; // send and end the program
+    return 0; // tells the program it worked withouth erros
+}
